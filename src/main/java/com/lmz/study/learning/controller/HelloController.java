82b7a3faaf652b6020020c16d00181e8b5b6bcf0
limingzhe <13631373091@163.com>
2019-05-22 11:59:04 +0800

    codemzl

---
+++ src/main/java/com/lmz/study/learning/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.lmz.study.learning.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Author: lmz
+ * @Date: 2019/5/21 17:01
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
